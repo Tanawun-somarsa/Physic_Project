8e042ccadffee62568e6fb60a6098c755de21828
PeerG7 <peerapon.lukc@bumail.net>
2025-03-29 00:18:22 +0700

    Alot

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,16 +1,46 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform[] spawnpoint;
+    public GameObject EnemyPrefab;
+    public GameObject EnemyPrefabRed;
+
+    public Wave[] waves;
+    public int currentWave = 0;
+
     void Start()
     {
-        
+        StartCoroutine(SpawnRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    void Spawn()
     {
-        
+        int ballon = Random.Range(0, spawnpoint.Length);
+        Instantiate(EnemyPrefab, spawnpoint[ballon].position, Quaternion.identity);
+        int ballonRed = Random.Range(0, spawnpoint.Length);
+        Instantiate(EnemyPrefab, spawnpoint[ballonRed].position, Quaternion.identity);
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while(currentWave < waves.Length)
+        {
+            Debug.Log($"Wave: {currentWave + 1}");
+            Wave wave = waves[currentWave];
+            yield return new WaitForSeconds(wave.Delaystart);
+
+            for (int i = 0; i < wave.totalSpawnEnemies; i++)
+            {
+                int EnemyIndex = Random.Range(0, wave.numberOfRandomSpawn);
+                Instantiate(EnemyPrefab, spawnpoint[EnemyIndex].position, Quaternion.identity);
+                yield return new WaitForSeconds(wave.spawnInterval);
+            }
+
+            Debug.Log("Next Wave");
+            currentWave++;
+        }
+        Debug.Log("Finished!!");
     }
 }
